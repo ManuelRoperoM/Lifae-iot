ae3d0ebb86f63aa78f479d61be1caab7872a9ee4
ManuelRoperoM <mnlrpr@gmail.com>
2024-03-06 13:52:40 -0500

    Manejo http sin espera del servidor

--- SendData/SendData.ino
+++ SendData/SendData.ino
@@ -1,13 +1,11 @@
 #include <WiFi.h>
-#include <HTTPClient.h>
-
+//sudo chmod 666 /dev/ttyUSB0
 const char* ssid = "MANUEL_99";
 const char* password = "Barbara1032502994";
-const char* serverAddress = "http://192.168.1.15:8081/random-data";  // Puedes usar "localhost" en lugar de la dirección IP
+const char* serverAddress = "192.168.1.15";  // Puedes usar "localhost" en lugar de la dirección IP
 const int serverPort = 8081;  // Ajusta el puerto para que coincida con el de tu servidor
 
 void setup() {
-  Serial.begin(115200);
   connectToWiFi();
 }
 
@@ -22,44 +20,28 @@ void loop() {
 
 void sendToBackend(float data) {
   WiFiClient client;
-  HTTPClient http;
 
-  Serial.println("Conectando al servidor backend local...");
+  Serial.println("Conectando al servidor...");
 
-  if (http.begin(client, serverAddress)) {
-    // Configura la cabecera y el contenido del mensaje POST
-    http.addHeader("Content-Type", "application/json");
+  if (client.connect(serverAddress, serverPort)) {
 
-    // Crea un objeto JSON con el dato aleatorio
     String jsonPayload = "{\"number\": " + String(data) + "}";
 
-    // Realiza la solicitud POST con el JSON como contenido
-    int httpCode = http.POST(jsonPayload);
-
-    if (httpCode > 0) {
-      String payload = http.getString();
-      Serial.println("Respuesta del servidor backend local: " + payload);
-    } else {
-      Serial.println("Error en la solicitud POST al servidor backend local");
-    }
-
-    // Libera recursos
-    http.end();
-  } else {
-    Serial.println("Error al conectarse al servidor backend local");
+    client.println("POST /random-data HTTP/1.1");
+    client.println("Host: " + String(serverAddress));
+    client.println("Content-Type: application/json");
+    client.println("Content-Length: " + String(jsonPayload.length()));
+    client.println();
+    client.print(jsonPayload);
+    client.stop();
   }
 }
 
 void connectToWiFi() {
-  Serial.println("Conectando a WiFi");
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.println("Conectando...");
   }
 
-  Serial.println("Conectado a WiFi");
-  Serial.print("Dirección IP: ");
-  Serial.println(WiFi.localIP());
 }
